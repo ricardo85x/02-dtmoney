f6116e8fcbeedaa1f2ffe465e5a3fb7352eab9fb
Ricardo <ricardoinliner@yahoo.com>
2021-03-22 09:52:22 -0300

    installed styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import styled from 'styled-components'
+
+
+const Title = styled.h1`
+
+  font-size: 64px;
+  color: #8257e6;
+
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Ola dtmoney</h1>
+      <Title>Ola dtmoney</Title>
     </div>
   );
 }
